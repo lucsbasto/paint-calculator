46eeb990bbfc2d947a689a384e686f37fa914fbb
Lucas Bastos <lucas.b@adireto.com>
2021-03-01 16:56:39 -0300

    feat: add wall methods

--- src/wall/wall.service.ts
+++ src/wall/wall.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Wall } from 'src/utils/interfaces/wall.interface';
 
 const DOOR =  {
   height: 1.9,
@@ -14,4 +15,50 @@ const WINDOW = {
 export class WallService {
   private windowArea = WINDOW.height * WINDOW.width;
   private doorArea = DOOR.height * DOOR.width;
+  
+  isWindowFit(wall: Wall) {
+    const windowArea = this.getWindowArea(wall);
+    const doorArea = this.getDoorArea(wall);
+    const wallArea = this.getWallArea(wall);
+    const isFit = (windowArea + doorArea) < (wallArea * 0.5) ? true : false;
+    return isFit;
+  }
+
+  isDoorFit({height}: Wall){
+    return height > DOOR.height * 0.3 
+  }
+
+  getWallArea({height, width}: Wall){
+    return height * width
+  }
+
+  getWindowArea({ window }: Wall){
+    return this.windowArea * window;
+  }
+
+  getDoorArea({ door }: Wall){
+    return this.doorArea * door;
+  }
+
+  verifyWallHeigthAndWidth({height, width}: Wall){
+    if(height < 1 || height > 15 || width < 1 || width > 15){
+      return false
+    }
+    return true
+  }
+
+  getPaintableArea(wall: Wall){
+    const windowArea = this.getWindowArea(wall);
+    const doorArea = this.getDoorArea(wall);
+    const wallArea = this.getWallArea(wall);
+
+    const paintableArea = wallArea - doorArea - windowArea;
+    return paintableArea;
+  }
+
+  paint(wall: Wall){
+    const AREA_PAINTED_PER_LITER = 5;
+    const paintableArea = this.getPaintableArea(wall);
+    return paintableArea / AREA_PAINTED_PER_LITER;
+  }
 }
